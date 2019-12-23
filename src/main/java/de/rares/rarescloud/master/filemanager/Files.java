3cf750dda1881018807eee5a5b5adfd11e97a7e7
Rares <rsahleanu@gmail.com>
2019-12-23 22:03:29 +0100

    Basic-3  ##Update

---
+++ src/main/java/de/rares/rarescloud/master/filemanager/Files.java
@@ -0,0 +1,13 @@
+package de.rares.rarescloud.master.filemanager;
+
+import java.io.*;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+
+public class Files {
+
+    public static void update()  {
+       
+    }
+
+}
